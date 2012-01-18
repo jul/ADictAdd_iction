882bedefd5a4801a001617e08c894ab3f7e2fac2
julien tayon <percol@ti0n.net>
2012-01-18 17:45:18 +0100

    a little bit of doc

--- example/dict_dist.py
+++ example/dict_dist.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
 # -*- encoding=utf-8 -*-
+"""
+Simple test of dictionnary distance with strings being casted as
+vectors of letters. And comparaison of misc candidates to the "reference"
+3 measures are used : 
+cos : aka vectors pointing in the same direction
+projection : one vector is projected on another one, and we normalize
+on the norm of the reference, the closest to one are best fits
+difference : each vectors are substracted and we compare the norms
+
+"""
 
 from vector_dict import VectorDict
 
 
 def closest_to_one(a_list, **arg):
+    """return a sorted
+    based on comparing values that are in absolute values 
+    the closest to one"""
     return sorted(
         a_list,
         cmp = lambda x, y: cmp(
@@ -16,6 +29,7 @@ def closest_to_one(a_list, **arg):
 
 
 def closest( candidate ):
+    """ misc test of distance"""
     cand_vect = string_as_vect(candidate)
     print "** for the word <%s>" % candidate
 
@@ -68,9 +82,6 @@ could_be = [
 
 
 vectors =  dict( ( noun, string_as_vect(noun) ) for noun in could_be )
-#for k,v in vectors.iteritems():
-#    print "%s is " % k 
-#    v.pprint()
 
 map( closest, [ u"com", u"misère", u"commis", u"signifiant", u"komisaire",
     u"lol", u"mizerre", u"chromosome", u"noscomiale", u"économique" ] )
