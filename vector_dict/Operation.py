e40c88c245802ab33888c2b28ca4e4c80cf15b78
julien tayon <percol@ti0n.net>
2012-02-04 17:47:30 +0100

    operation for matrix, should be in a subspace maybe ?

---
+++ vector_dict/Operation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""misc function that can be used on tree or leaf
+just for convenience
+
+identity : return itself
+copy : return a copy of a value
+mul(by) : return a function that multiples by a value
+cast(type) : cast the element in given type
+"""
+
+identity = lambda x : x 
+copy = lambda x : x.copy()
+mul = lambda by :  lambda x : x * by 
+cast = lambda _type : lambda x : _type(x)
