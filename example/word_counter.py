4afd9b92291e9447e8203fbbdc23581840c4a9f9
julien tayon <jtayon@gmail.com>
2012-04-09 15:27:46 +0200

    results added

--- example/word_counter.py
+++ example/word_counter.py
@@ -54,5 +54,80 @@ print "\n".join(
     
 )
 
-
+"""
+EXPECTED RESULTS : 
+Frequency of words begining with
+{
+    '\xc3' : 4,
+    'r' : 1426,
+    'a' : 5372,
+    '1' : 10,
+    'c' : 2778,
+    'b' : 2659,
+    'e' : 1420,
+    'd' : 2564,
+    'g' : 1534,
+    'f' : 2647,
+    'i' : 942,
+    'h' : 5867,
+    'k' : 483,
+    'j' : 245,
+    'm' : 2567,
+    'l' : 2706,
+    'o' : 1669,
+    'n' : 1416,
+    'q' : 199,
+    'p' : 2173,
+    's' : 5434,
+    '2' : 2,
+    'u' : 407,
+    't' : 9255,
+    'w' : 5491,
+    'v' : 507,
+    'y' : 2077,
+    'x' : 9,
+    'z' : 3,
+}
+TOP 40 most used words
+       the=3786
+       and=2216
+       you=1446
+      that=1362
+       was=1083
+       his=995
+       had=833
+      with=661
+       him=661
+       for=588
+      have=561
+       not=474
+       her=439
+       she=431
+    dorian=420
+      what=399
+       but=396
+       one=393
+       are=372
+     there=337
+      they=318
+     would=308
+       all=294
+      said=262
+       don=255
+      from=254
+      were=251
+      lord=248
+     henry=237
+      been=236
+      life=232
+      like=227
+       who=224
+     about=223
+      when=218
+      your=207
+      some=205
+      gray=205
+      them=203
+      will=201
+"""
 
